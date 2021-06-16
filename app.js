81a1601d50b3b9c1ad4270669682e06421b84a88
Rene Rhoen <renerhoen@hotmail.com>
2021-06-16 21:04:03 +0900

    did again the main logic from scratch. added neumorphism design

--- app.js
+++ app.js
@@ -1,63 +1,33 @@
-// get the element to insert text
-const typingDiv = document.querySelector('#typing');
-let text, characters;
+const textDiv = document.querySelector('#typing');
 
-fetch('./text.json')
-    .then((res) => {
-        return res.json();
-    })
-    .then((data) => {
-        text = data.python;
-        // to make sure that the game don't start before the data is loaded
-        startGame();
-    });
-
-const startGame = () => {
-    shuffledSentence(text);
-    console.log('text shuffled: ', characters);
-    console.log('type of characters: ', typeof characters);
-
-    // using ... turns the string into array so we can map it
-    [...characters].map((char) => {
-        const span = document.createElement('span');
-        span.innerText = char;
-        typingDiv.appendChild(span);
-        return span;
-    });
-
-    let cursorIndex = 0;
-    let cursorCharacter = characters[cursorIndex];
-    console.log('cursor char: ', cursorCharacter);
-
-    // !!! giving cursorCharacter.classList is undefined ...
-
-    cursorCharacter.classList.add('cursor');
-
-    document.addEventListener('keydown', ({ key }) => {
-        if (key === cursorCharacter.innerText) {
-            cursorCharacter.classList.remove('cursor');
-            cursorCharacter.classList.add('done');
-            cursorCharacter = characters[++cursorIndex];
-            cursorCharacter.classList.add('cursor');
-        }
-    });
+const text = {
+    python: 'False await ( else , import . pass None " break except in : raise True [ class finally is return and continue for lambda try as def from nonlocal while assert del global __ not with async elif _ if or yield __init__',
 };
 
-const shuffledSentence = (text) => {
-    characters = text.split(' ').shuffle().join(' ');
-    return characters;
-};
+const character = text.python.split('').map((char) => {
+    const span = document.createElement('span');
+    span.innerText = char;
+    textDiv.appendChild(span);
+    return span;
+});
 
-// Fisher-Yates shuffle method
-Array.prototype.shuffle = function () {
-    var i = this.length;
-    if (i == 0) return this;
-    while (--i) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var a = this[i];
-        var b = this[j];
-        this[i] = b;
-        this[j] = a;
+let cursorIndex = 0;
+let currentChar = character[cursorIndex];
+currentChar.classList.add('cursor');
+
+let errorsQtd = 0;
+
+document.addEventListener('keydown', ({ key }) => {
+    if (key === currentChar.innerText) {
+        currentChar.classList.remove('cursor');
+        console.log(key);
+        currentChar.classList.add('correct');
+        currentChar = character[++cursorIndex];
+        currentChar.classList.add('cursor');
     }
-    return this;
-};
+    // if (key !== currentChar.innerText) {
+    //     currentChar.classList.remove('cursor');
+    //     currentChar.classList.add('wrong');
+    //     errorsQtd += 1;
+    // }
+});
