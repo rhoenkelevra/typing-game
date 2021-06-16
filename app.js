f5a5b32b2050afbe4d1eac9b64890e1f1144a41b
Rene Rhoen <renerhoen@hotmail.com>
2021-06-16 22:34:59 +0900

    start working on timer function

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+/**
+ * Todo:
+ * header timer
+ * errors count and render red char on mistake
+ * save records in local storage
+ * display correct date
+ */
+
 const textDiv = document.querySelector('#typing');
+const dateDiv = document.querySelector('#date');
+const wpmDiv = document.querySelector('#wpm');
+const cpmDiv = document.querySelector('#cpm');
+const timeDiv = document.querySelector('#time');
+const errorsDiv = document.querySelector('#errors');
 
 const text = {
-    python: 'False await ( else , import . pass None " break except in : raise True [ class finally is return and continue for lambda try as def from nonlocal while assert del global __ not with async elif _ if or yield __init__',
+    python: 'False await, (param) else , import . pass None" break except in : raise True [ class finally is return and continue for lambda try as def from nonlocal while assert del global __ not with async elif _ if or yield __init__',
 };
 // const shuffledSentence = (text) => {
 //     characters = text.split(' ').shuffle().join(' ');
 //     return characters;
 // };
 
-const character = text.python.split('').map((char) => {
+const characters = text.python.split('').map((char) => {
     const span = document.createElement('span');
     span.innerText = char;
     textDiv.appendChild(span);
@@ -16,22 +29,53 @@ const character = text.python.split('').map((char) => {
 });
 
 let cursorIndex = 0;
-let currentChar = character[cursorIndex];
+let currentChar = characters[cursorIndex];
 currentChar.classList.add('cursor');
-
+const numberOfWords = text.python.split(' ').length;
+const numberOfChars = text.python.split('').length;
+console.log(numberOfWords);
+console.log(numberOfChars);
 let errorsQtd = 0;
+let startTime = null;
+let endTime = null;
 
-document.addEventListener('keydown', ({ key }) => {
+const typingEvent = document.addEventListener('keydown', ({ key }) => {
+    if (!startTime) {
+        startTime = new Date();
+        //startTimer();
+    }
     if (key === currentChar.innerText) {
         currentChar.classList.remove('cursor');
-        console.log(key);
+
         currentChar.classList.add('correct');
-        currentChar = character[++cursorIndex];
-        currentChar.classList.add('cursor');
+        currentChar = characters[++cursorIndex];
     }
-    // if (key !== currentChar.innerText) {
-    //     currentChar.classList.remove('cursor');
-    //     currentChar.classList.add('wrong');
-    //     errorsQtd += 1;
-    // }
+    if (cursorIndex >= characters.length) {
+        endTime = new Date();
+        const delta = endTime - startTime;
+        const seconds = delta / 1000;
+        const numberOfWords = text.python.split(' ').length;
+        const numberOfChars = text.python.split('').length;
+        const cpm = Math.floor((numberOfChars / seconds) * 60);
+        const wpm = Math.floor((numberOfWords / seconds) * 60);
+        cpmDiv.innerText = `${cpm} chars per minute`;
+        wpmDiv.innerText = `${wpm} words per minute`;
+        document.removeEventListener('keydown', typingEvent);
+        return;
+    }
+    currentChar.classList.add('cursor');
 });
+
+// Timer
+// const timeLimit = 60
+// let timePassed = 0
+// let timerInterval = null
+// let timeLeft;
+
+// function startTimer() {
+//    timeInterval = setInterval(() => {
+// 	   timePassed = timePassed += 1;
+// 	   timeLeft = timeLimit - timePassed
+
+//    }, 1000)
+// }
